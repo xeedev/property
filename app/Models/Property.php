eb406c3fc3ae80a17edce62affee471d57b6e88e
xeemax <xeemax2.0@gmail.com>
2022-07-18 22:50:52 +0500

    added relationships

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -16,4 +16,16 @@ class Property extends Model
         'sold_by_user_id',
         'sold_to_user_id'
     ];
+    public function location(){
+        return $this->belongsTo(Location::class);
+    }
+    public function sold_to(){
+        return $this->belongsTo(User::class,'sold_to_user_id');
+    }
+    public function sold_by(){
+        return $this->belongsTo(User::class,'sold_by_user_id');
+    }
+    public function media(){
+        return $this->morphMany('App\Models\Media', 'imageable');
+    }
 }
