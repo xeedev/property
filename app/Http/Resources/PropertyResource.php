88c0828a6937f3efae2352ed889150842f6299a8
xeemax <xeemax2.0@gmail.com>
2022-08-14 20:57:37 +0500

    updated property resource

--- app/Http/Resources/PropertyResource.php
+++ app/Http/Resources/PropertyResource.php
@@ -18,6 +18,8 @@ class PropertyResource extends JsonResource
             'id' => $this->id,
             'property_number' => $this->property_number,
             'detail' => $this->detail,
+            'demand' => $this->demand,
+            'negotiated_price' => $this->negotiated_price,
             'status' => $this->status,
             'location'  => $this->location,
             'block'  => $this->block,
