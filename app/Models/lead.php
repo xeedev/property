ee5ad691c3bc9787b970d3361dbfe77e0c114149
xeemax <xeemax2.0@gmail.com>
2022-08-16 19:41:01 +0500

    Added relations in leads model

--- app/Models/lead.php
+++ app/Models/lead.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,4 +18,13 @@ class lead extends Model
         'commission_received',
         'sold_in'
     ];
+    public function sold_to(){
+        return $this->belongsTo(User::class,'sold_to_user_id');
+    }
+    public function sold_by(){
+        return $this->belongsTo(User::class,'sold_by_user_id');
+    }
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->format('d-m-Y');
+    }
 }
