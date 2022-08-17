b8055b7ffee670e7c7eab1c6fef95778688df9f6
xeemax <xeemax2.0@gmail.com>
2022-08-17 23:16:16 +0500

    added artisan route to link storage on server

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/linkstorage', function () {
+    $exitCode = \Illuminate\Support\Facades\Artisan::call('storage:link', [] );
+    echo $exitCode;
+});
 Route::get('/{path}', \App\Http\Controllers\FrontendController::class)
     ->where('path', '^(?!dashboard|admin|api|nova-api|pusher|nova-vendor).*');
