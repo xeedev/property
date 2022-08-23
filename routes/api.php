de8751c076f6e097e5d928a5b69cc863b861efc7
xeemax <xeemax2.0@gmail.com>
2022-08-23 22:49:55 +0500

    added maps resource api

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,14 @@ Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 
 Route::middleware('auth:api')->group( function () {
+    Route::resource('maps', \App\Http\Controllers\API\MapsController::class);
     Route::resource('properties', \App\Http\Controllers\API\PropertyController::class);
     Route::resource('locations', \App\Http\Controllers\API\LocationController::class);
     Route::resource('blocks', \App\Http\Controllers\API\BlockController::class);
     Route::resource('leads', \App\Http\Controllers\API\LeadController::class);
     Route::resource('users', \App\Http\Controllers\API\UserController::class);
     Route::post('imageUpload',[\App\Http\Controllers\API\PropertyController::class,'imageUpload']);
+    Route::post('fileUpload',[\App\Http\Controllers\API\MapsController::class,'fileUpload']);
     Route::post('logout', [RegisterController::class, 'logout']);
     Route::get('/validate-token', function (Request $request) {return response()->json(['authenticated' => true]);});
     Route::get('statistics',[\App\Http\Controllers\API\PropertyController::class,'getStatistics']);
